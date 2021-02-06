e734cf2df5a655fff0de108510c85ef4e116760b
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-02-06 01:13:05 +0100

    har försök komma igång med javascript

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let pieces = [
+    {
+        id: 0
+        position 0
+    }
+]
+
+let redpiceses = [0,0,0,0];
+let redpiceses = [0,0,0,0];
+let redpiceses = [0,0,0,0];
+let redpiceses = [0,0,0,0];
+
+
+function movePience(piece, steps){
+   let dice = Math.random() *6 +1;
+}
+
